af0453dae8516d29aba0db7d78fbbd395a26938a
eros14 <te14.oscar.eriksson@umea.nti.se>
2016-09-02 11:24:38 +0200

    Changed some stuff

---
+++ src/ReverseNumbers.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Oscar Eriksson on 2016-09-02.
+ */
+public class ReverseNumbers {
+
+public static void main (String[]args) {
+
+    String[] List = ("1", "2", "3", "4");
+
+    for(int i;) {
+
+        System.out.println(List);
+    }
+    }
+}
